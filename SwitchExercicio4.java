869201c145879d91fa6c50c299c92c40c08472b3
Patrícia Rocha <ptc.patirocha@gmail.com>
2026-04-22 23:51:35 -0300

    Segundo commit: envio do exercício 8

--- SwitchExercicio4.java
+++ SwitchExercicio4.java
@@ -1,9 +1,42 @@
 package atividadePraticaJava2;
 
+import java.util.Scanner;
+
 public class SwitchExercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int operacao;
+		float saldo = 1000;
+		float deposito, saque, novoSaldo;
+		
+		System.out.println("Digite o número da operação: ");
+		operacao = leia.nextInt();
+		
+		switch (operacao) {
+		
+		case 1:
+			System.out.println("O seu saldo é: " + saldo);
+			break;
+			
+		case 2:
+			System.out.println("Digite o valor que deseja sacar: ");
+			saque = leia.nextFloat();
+			novoSaldo = saldo - saque;
+			break;
+			
+		case 3:
+			System.out.println("Digite o valor que deseja depositar: ");
+			deposito = leia.nextFloat();
+			novoSaldo = saldo + deposito;
+			break;
+			
+		default:
+		    System.out.println("Operação Inválida!");
+		    break;
+		}
 
 	}
 
